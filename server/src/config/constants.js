99fc44ae93293baeb701c4038efbd61ed076103a
Florian <florian.bouron@insa-lyon.fr>
2017-08-07 21:08:20 +0200

    Add JWT

--- server/src/config/constants.js
+++ server/src/config/constants.js
@@ -2,4 +2,5 @@ export default {
   PORT: process.env.PORT || 3000,
   DB_URL: 'mongodb://localhost/twitter-development',
   GRAPHQL_PATH: '/graphql',
+  JWT_SECRET: 'FD45fd9q6ssMna54dsf'
 };
--- server/src/index.js
+++ server/src/index.js
@@ -1,37 +1,17 @@
 /* eslint-disable no-console */
 
 import express from 'express';
-import bodyParser from 'body-parser';
-import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
-import { makeExecutableSchema } from 'graphql-tools';
 import { createServer } from 'http';
 
-import './config/db'
+import './config/db';
 import constants from './config/constants';
-import typeDefs from './graphql/schema';
-import resolvers from './graphql/resolvers';
+import middlewares from './config/middlewares';
 import mocks from './mocks';
 
 const app = express();
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-});
 
-app.use(bodyParser.json());
+middlewares(app);
 
-app.use(
-  '/graphiql',
-  graphiqlExpress({
-    endpointURL: constants.GRAPHQL_PATH,
-  }),
-);
-app.use(
-  constants.GRAPHQL_PATH,
-  graphqlExpress({
-    schema,
-  }),
-);
 const graphQLServer = createServer(app);
 
 mocks().then(() => {
